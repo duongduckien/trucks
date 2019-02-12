8cd6f820b4cf268867e3e03de02a127fa768234d
DuongKien <ddk0705@gmail.com>
2019-02-13 00:16:07 +0700

    Create router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Components
 import Home from '../containers/Home';
 import Login from '../containers/Login';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export class Main extends Component<{}> {
 
     render() {
         return (
             <div className='container'>
+                <Header />
                 <Router>
                     <Switch>
                         <Route path="/" exact component={Home} />
                         <Route path="/login" exact component={Login} />
                     </Switch>
                 </Router>
+                <Footer />
             </div>
         )
     }
